5e4066a7779179c79b35efc1eb8360100c1c16dd
николай <137291429+pro11082007G@users.noreply.github.com>
2023-08-09 14:20:03 +0800

    Update lesson_4.1.py

--- lesson_4.1.py
+++ lesson_4.1.py
@@ -1,30 +1,3 @@
-'''def strcounter(string): # сложность O(n**2)
-    for symbol in string:
-        counter = 0
-        for sub_symbol in string:
-            if symbol == sub_symbol:
-                counter += 1
-        print(symbol, '-',counter)
-
-strcounter('abaxcvcxccvcde')
-
-def strcounter(string): # сложность O(n*m)
-    for symbol in set(string):
-        counter = 0
-        for sub_symbol in string:
-            if symbol == sub_symbol:
-                counter += 1
-        print(symbol, '-', counter)
-
- strcounter('abaxcvcxccvcde')'''
-
-# def strcounter(string): #сложность O(n)
-#     syms_counter = {}
-#     for symbol in string:
-#         syms_counter[symbol] = syms_counter.get(symbol, 0)+1
-#     print(syms_counter)
-# strcounter('abeaxcvcxccvcd')
-
 
 word = str(input('Введите слово:'))
 a = word[::-1]
